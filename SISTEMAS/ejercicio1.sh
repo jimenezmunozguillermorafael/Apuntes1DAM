e3dc39f9d3325fe8ac32897e182cc330384a3848
Guillermo <jimenezmunozguillermorafael001.alu@stemgranada.com>
2025-09-26 21:20:08 +0200

    .

---
+++ SISTEMAS/ejercicio1.sh
@@ -0,0 +1,4 @@
+
+##mkdir -p = Crea un árbol completo de directorios si no existen. Esto es útil para crear varios niveles de directorios de una sola vez.
+##Pasos por separado: mkdir PT0, touch file1, mkdir1 dir2, touch file2 , cd dir 2, mkdir dir 2 dir3
+##----(DESPUES DE LA RUTA INICIAL HASTA DONDE VAS A CREAR "PT0")--- Solo un comando: mkdir PT0; cd PT0; touch file1; mkdir dir1; cd dir1; mkdir dir2; touch file2; cd dir2; mkdir dir3 dir4
