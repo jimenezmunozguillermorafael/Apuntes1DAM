68c67ee0261a932e7a109367c1b2fce9178f3204
Guillermo <jimenezmunozguillermorafael001.alu@stemgranada.com>
2025-09-30 21:19:17 +0200

    .

--- SISTEMAS/Repositorio2/Ejercicio1.sh
+++ SISTEMAS/Repositorio2/Ejercicio1.sh
@@ -29,4 +29,12 @@ proyecto/
 #!/bin/bash
 ## Ir a la ruta y despues crear 
 mkdir -p proyecto/{docs,src/modulos,config,logs/backup,test/resultados}
-touch /home/alumnoT/Documentos/GithubTarde/proyecto/docs {manual.txt,guia.txt,referencnias.txt},src {script1.sh,script2.sh}
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/docs/{manual.txt,guia.txt,referencnias.txt}; 
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/src/{script1.sh,script2.sh}; 
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/src/modulos/modulo1.py;
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/config/{app.cfg,db.cfg};
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/logs/{acecess.log,error.log};
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/logs/backup/backup1.log;
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/test/{test1.txt,test2.txt}
+touch /home/alumnoT/Documentos/GithubTarde/proyecto/test/resultados/resultados1.txt
+ 
